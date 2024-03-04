fd608ff542ca00c6050c8ad456cc93093d9eefae
Sarabjeet Singh <mrsinghspecialist@gmail.com>
2024-03-04 16:13:24 +0530

    Created HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
 import HomeHeader from "@/components/HomeHeader/HomeHeader";
+import HomeSearch from "@/components/HomeSearch/HomeSearch";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <HomeHeader />
+      <div className="flex flex-col items-center mt-24">
+        <Image
+          src={
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1280px-Google_2015_logo.svg.png"
+          }
+          width={300}
+          height={100}
+          alt={"Google Logo"}
+        />
+        <HomeSearch />
+      </div>
     </>
   );
 }
