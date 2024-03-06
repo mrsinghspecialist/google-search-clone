ab9b14cc21030397b01a22b14c4d8fd773b30030
Sarabjeet Singh <mrsinghspecialist@gmail.com>
2024-03-06 15:20:33 +0530

    Complete the SearchHeaderOptions component

--- src/components/SearchHeader/SearchHeader.tsx
+++ src/components/SearchHeader/SearchHeader.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import SearchBox from "./SearchBox";
 import { RiSettings3Line } from "react-icons/ri";
 import { TbGridDots } from "react-icons/tb";
+import SearchHeaderOptions from "./SearchHeaderOptions";
 
 export default function SearchHeader() {
   return (
@@ -32,6 +33,7 @@ export default function SearchHeader() {
           SignIn
         </button>
       </div>
+      <SearchHeaderOptions />
     </header>
   );
 }
