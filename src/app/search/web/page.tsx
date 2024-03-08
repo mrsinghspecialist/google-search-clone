d7bc3ab9ac48efbb6c25764e099ba62a1eb6f150
Sarabjeet Singh <mrsinghspecialist@gmail.com>
2024-03-08 15:39:52 +0530

    Handle possible errors

--- src/app/search/web/page.tsx
+++ src/app/search/web/page.tsx
@@ -1,5 +1,40 @@
-import React from "react";
+import data from "@/assets/searchData.json";
+import Link from "next/link";
 
-export default function WebSearchPage() {
-  return <div>WebSearchPage</div>;
+export default async function WebSearchPage({
+  searchParams,
+}: {
+  searchParams: { searchTerm: string };
+}) {
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.NEXT_PUBLIC_SEARCH_API_KEY}&cx=${process.env.NEXT_PUBLIC_CONTEXT_KEY}&q=${searchParams.searchTerm}`
+  );
+  if (!response.ok) {
+    // throw new Error("Something went wrong");
+  }
+  // const data = await response.json();
+  let results = data.items;
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4">
+          No results found for {searchParams.searchTerm}
+        </h1>
+        <p className="text-lg">
+          Try searching the web or images for something else&nbsp;
+          <Link href="/" className="text-blue-500">
+            Home
+          </Link>
+        </p>
+      </div>
+    );
+  }
+  return (
+    <div>
+      {results &&
+        results.map((result: any) => {
+          return <h1>{result.title}</h1>;
+        })}
+    </div>
+  );
 }
